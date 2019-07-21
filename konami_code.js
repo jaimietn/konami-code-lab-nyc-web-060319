b754e639d408b046c089db6c1c491415148b3168
github username <github email address>
2019-07-21 13:29:06 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,38 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+
+    index = (codes[index] === key) ? ++index : 0
+
+    if (index === codes.length) {
+      window.alert("Hurray!");
+      index = 0
+    }
+
+  });
 }
+
+//my code below wasnt working, JS hell with curly boys
+// function init() {
+//   // your code here
+//   let index = 0
+//
+//   document.body.addEventListener('keydown', (e) {
+//     const key = e.key
+//
+//     if (key === codes[index]) {
+//       index++
+//
+//       if (index === codes.length) {
+//         alert('Congratulations!')
+//       }
+//     }
+//     else {
+//       index = 0
+//     }
+//   })
+// }
